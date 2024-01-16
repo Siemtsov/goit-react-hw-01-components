636ad0d46fd6632803c1aa1c577810f527c6ed14
Ivan <vaniysemtsov@gmail.com>
2024-01-16 17:05:14 +0200

    task-01 done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,24 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React from 'react';
+import user from '../components/data/user.json';
+import Profile from './Profile/profile';
+
+// import data from '../data/data.json';
+// import Statistics from './Statistics/Statistics';
+
+// import friends from '../data/friends.json';
+// import FriendList from './FriendList/FriendList';
+
+// import transactions from '../data/transactions.json';
+// import TransactionHistory from './TransactionHistory/TransactionHistory';
+
+export const App = () => (
+  <>
+    <Profile
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
+    />
+  </>
+);
